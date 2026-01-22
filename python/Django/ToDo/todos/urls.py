3f70addfdd73a93c584cb5e8b16aa637f69b4153
Suraj <suraj.prajapati@altysys.com>
2026-01-22 16:17:07 +0530

    uploading the delete, update, show logic

--- python/Django/ToDo/todos/urls.py
+++ python/Django/ToDo/todos/urls.py
@@ -2,5 +2,18 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('addtask/', views.addTask, name='addTask')
+
+    # Add Task
+    path('addtask/', views.addTask, name='addTask'),
+
+    # Mark as done feature
+    path('mark_as_done/<int:pk>/', views.mark_as_done, name='mark_as_done'),
+
+    # Mark as undone feature 
+    path('mark_as_undone/<int:pk>/', views.mark_as_undone, name='mark_as_undone'),
+
+    # Edit features 
+    path('edit_task/<int:pk>/', views.edit_task, name='edit_task'),
+        # Delete task
+    path('delete_task/<int:pk>/', views.delete_task, name='delete_task'),
 ]
